4a4222a2c68cd6879de3489d9727d26d43adc54e
sajjadnejati98 <sajjadnejati.98@gmail.com>
2025-09-23 15:49:36 +0330

    Update bot_webhook.py

--- bot_webhook.py
+++ bot_webhook.py
@@ -1,28 +1,23 @@
-import asyncio
-import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
 TOKEN = "8208186251:AAGhImACKTeAa1pKT1cVSQEsqp0Vo2yk-2o"
-PORT = int(os.environ.get("PORT", 5000))
-HOST = os.environ.get("RENDER_EXTERNAL_HOSTNAME")  # آدرس Render
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("ربات فعال است ✅")
 
-async def main():
-    app = Application.builder().token(TOKEN).build()
+if __name__ == "__main__":
+    # ساخت اپلیکیشن
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    # اضافه کردن دستور /start
     app.add_handler(CommandHandler("start", start))
 
-    webhook_url = f"https://{HOST}/{TOKEN}"
-    await app.bot.set_webhook(webhook_url)  # وبهوک رو ست می‌کنیم
-
-    # اجرای ربات روی وبهوک
-    await app.run_webhook(
+    # اجرای وبهوک
+    webhook_url = f"https://unix-glass-bot-1.onrender.com/{TOKEN}"
+    app.run_webhook(
         listen="0.0.0.0",
-        port=PORT,
-        webhook_url=webhook_url  # جایگزین webhook_path
+        port=int(os.environ.get("PORT", 10000)),
+        webhook_url=webhook_url
     )
-
-if __name__ == "__main__":
-    asyncio.run(main())
